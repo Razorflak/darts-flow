77d9adb0852777121c7088de66f3abb7f3a0c73d
Julien T. <tanguyj35@gmail.com>
2025-11-19 18:37:22 +0100

    Modification url backend

--- packages/front-end-stream-tool/src/lib/requester/utils.ts
+++ packages/front-end-stream-tool/src/lib/requester/utils.ts
@@ -1,6 +1,5 @@
-import { env } from "$env/dynamic/public";
-
 export const getApiBaseUrl = (): string => {
-	const apiBaseUrl = env.PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+	const localIpUrl = `http://${window.location.hostname}:3000/api`;
+	const apiBaseUrl = localIpUrl || "http://localhost:3000/api";
 	return apiBaseUrl;
 };
